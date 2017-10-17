b3a6a5cd0617236e0603ebf1c65f84e3d4ad22b0
Sean Stappas <seanstappas@gmail.com>
2017-10-17 18:52:51 -0400

    More comments.

--- Assignment1/linear_networks.py
+++ Assignment1/linear_networks.py
@@ -47,6 +47,16 @@ def csv_to_network_branch_matrices(filename):
 
 
 def create_network_matrices_mesh(rows, cols, branch_resistance, test_current):
+    """
+    Create the network matrices needed (A, Y, J, E) to solve the resitive mesh network with the given rows, columns,
+    branch resistance and test current.
+
+    :param rows: the number of rows in the mesh
+    :param cols: the number of columns in the mesh
+    :param branch_resistance: the resistance in each branch
+    :param test_current: the test current to apply
+    :return: the network matrices (A, Y, J, E)
+    """
     num_horizontal_branches = (cols - 1) * rows
     num_vertical_branches = (rows - 1) * cols
     num_branches = num_horizontal_branches + num_vertical_branches + 1
@@ -59,6 +69,17 @@ def create_network_matrices_mesh(rows, cols, branch_resistance, test_current):
 
 
 def create_incidence_matrix_mesh(cols, num_branches, num_horizontal_branches, num_nodes, num_vertical_branches):
+    """
+    Create the incidence matrix given by the resistive mesh with the given number of columns, number of branches,
+    number of horizontal branches, number of nodes, and number of vertical branches.
+
+    :param cols: the number of columns in the mesh
+    :param num_branches: the number of branches in the mesh
+    :param num_horizontal_branches: the number of horizontal branches in the mesh
+    :param num_nodes: the number of nodes in the mesh
+    :param num_vertical_branches: the number of vertical branches in the mesh
+    :return: the incidence matrix (A)
+    """
     A = Matrix.empty(num_nodes, num_branches)
     node_offset = -1
     for branch in range(num_horizontal_branches):
@@ -86,18 +107,36 @@ def create_incidence_matrix_mesh(cols, num_branches, num_horizontal_branches, nu
     return A
 
 
-def create_network_branch_matrices_mesh(num_branches, resistance, test_current):
-    Y = Matrix.diagonal([1 / resistance if branch < num_branches - 1 else 0 for branch in range(num_branches)])
+def create_network_branch_matrices_mesh(num_branches, branch_resistance, test_current):
+    """
+    Create the Y, J, E network branch matrices of the resistive mesh given by the provided number of branches, branch
+    resistance and test current.
+
+    :param num_branches: the number of branches in the mesh
+    :param branch_resistance: the resistance of each branch in the mesh
+    :param test_current: the test current to apply to the mesh
+    :return: the Y, J, E network branch matrices
+    """
+    Y = Matrix.diagonal([1 / branch_resistance if branch < num_branches - 1 else 0 for branch in range(num_branches)])
     # Negative test current here because we assume current is coming OUT of the test current node.
     J = Matrix.column_vector([0 if branch < num_branches - 1 else -test_current for branch in range(num_branches)])
     E = Matrix.column_vector([0 for branch in range(num_branches)])
     return Y, J, E
 
 
-def find_mesh_resistance(n, branch_resistance, half_bandwidth=None):
+def find_mesh_resistance(N, branch_resistance, half_bandwidth=None):
+    """
+    Find the equivalent resistance of an Nx2N resistive mesh with the given branch resistance and optional
+    half-bandwidth
+
+    :param N:
+    :param branch_resistance:
+    :param half_bandwidth:
+    :return:
+    """
     test_current = 0.01
-    A, Y, J, E = create_network_matrices_mesh(n, 2 * n, branch_resistance, test_current)
+    A, Y, J, E = create_network_matrices_mesh(N, 2 * N, branch_resistance, test_current)
     x = solve_linear_network(A, Y, J, E, half_bandwidth=half_bandwidth)
-    test_voltage = x[2 * n - 1 if n > 1 else 0][0]
+    test_voltage = x[2 * N - 1 if N > 1 else 0][0]
     equivalent_resistance = test_voltage / test_current
     return equivalent_resistance
